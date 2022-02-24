e8ea31b8c4bd0619a0032e469eaf77ee6b361f66
dashabondarenko07 <dashabondarenko0709@gmail.com>
2022-02-24 20:45:53 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const body = document.querySelector('body');
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+let timerId = null;
 
+start.addEventListener('click', onClickStart);
+stop.addEventListener('click', onClickStop);
+
+function onClickStart() {
+  start.disabled = true;
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+function onClickStop() {
+  start.disabled = false;
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
